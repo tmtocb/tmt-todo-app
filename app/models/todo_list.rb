4a9a9b436b8c792a9b145a37c4329551a80c35af
tmt-mateusz <tmt.ocb@gmail.com>
2020-12-11 03:11:38 +0100

    add badge that change text and color depends on progress %

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -13,4 +13,26 @@ class TodoList < ApplicationRecord
   	return 0 if total_items == 0
   	( 100 * completed_items.to_f / total_items).round(1) 
   end
+
+  def status
+    case percent_complete.to_i
+    when 0
+      'Not started'
+    when 100
+      'Complete'
+    else 
+      'In progress'
+    end
+  end
+
+  def badge_color
+      case percent_complete.to_i
+    when 0
+      'dark'
+    when 100
+      'info'
+    else 
+      'primary'
+    end
+  end
 end
