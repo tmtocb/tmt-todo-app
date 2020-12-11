82667c689c86502649374c22dd862c223e8dff5e
tmt-mateusz <tmt.ocb@gmail.com>
2020-12-11 01:55:54 +0100

    make math behind progress bar work

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,3 +1,16 @@
 class TodoList < ApplicationRecord
   has_many :todo_items
+
+  def total_items
+  	@total_items ||= todo_items.count
+  end
+
+  def completed_items
+  	@completed_items ||= todo_items.completed.count
+  end
+
+  def percent_complete
+  	return 0 if total_items == 0 #avoid diving by 0
+  	( 100 * @completed_items.to_f / total_items).round(1) 
+  end
 end
